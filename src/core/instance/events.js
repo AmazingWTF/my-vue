48fcd45ad769eb57fb4903866285191dffd8017d
gwj <gwjlib@163.com>
2018-07-19 16:32:28 +0800

    event

---
+++ src/core/instance/events.js
@@ -0,0 +1,77 @@
+import { bind, toArray } from '../util'
+import { updateListeners } from '../vdom/helpers'
+
+export function initEvent (vm) {
+  vm._events = Object.create(null)
+  // init parent attached events
+  const listeners = vm.$options._parentListeners
+  const on = bind(vm.$on, vm)
+  const off = bind(vm.$off, vm)
+  vm._updateListeners = (listeners, oldListeners) => {
+    updateListeners(listeners, oldListeners || {}, on, off)
+  }
+  if (listeners) {
+    vm._updateListeners(listeners)
+  }
+}
+
+export function eventMixin (Vue) {
+  Vue.prototype.$on = function (event, fn) {
+    const vm = this
+    (vm._events[event] || (vm._events[event] = [])).push(fn)
+    return vm
+  }
+
+  Vue.prototype.$once = function (event, fn) {
+    const vm = this
+    function on () {
+      vm.$off(event, on)
+      fn.apply(vm, arguments)
+    }
+    on.fn = fn
+    vm.$on(event, on)
+    return vm
+  }
+
+  Vue.prototype.$off = function (event, fn) {
+    const vm = this
+    // all
+    if (!arguments.length)  {
+      vm._events = Object.create(null)
+      return vm
+    }
+    // specific evnet
+    const cbs = vm._events[event]
+    if (!cbs) {
+      return vm
+    }
+    if (arguments.length === 1) {
+      vm._events[event] = null
+      return vm
+    }
+    // specific handler
+    let cb
+    let i = cbs.length
+    while (i--) {
+      cb = cbs[i]
+      if (cb === fn || cb.fn === fn) {
+        cbs.splice(i, 1)
+        break
+      }
+    }
+    return vm
+  }
+
+  Vue.prototype.$emit = function (event) {
+    const vm = this
+    let cbs = vm._events[event]
+    if (cbs) {
+      cbs = cbs.length > 1 ? toArray(cbs) : cbs
+      const args = toArray(arguments, 1)
+      for (let i = 0, l = cbs.length; i < l; i++) {
+        cbs[i].apply(vm, args)
+      }
+    }
+    return vm
+  }
+}
---
+++ src/vdom/helpers.js
@@ -0,0 +1,37 @@
+export function updateListeners (on, oldOn, add, remove) {
+  let name, cur, old, fn, event, capture
+  for (name in on) {
+    cur = on[name]
+    old = oldOn[name]
+    if (!old) {
+      capture = name.charAt(0) === '!'
+      event = capture ? name.slice(1) : name
+      if (Array.isArray(cur)) {
+        add(event, cur.invoker = arrInvoker(cur), capture)
+      } else {
+        if (!cur.invoker) {
+          fn = cur
+          cur = on[name] = {}
+          cur.fn = fn
+          cur.invoker = fnInvoker(cur)
+        }
+        add(event, cur.invoker, capture)
+      }
+    } else if (cur !== old) {
+      if (Array.isArray(old)) {
+        old.length = cur.length
+        for (let i = 0; i < old.length; i++) old[i] = cur[i]
+        on[name] = old
+      } else {
+        old.fn = cur
+        on[name] = old
+      }
+    }
+  }
+  for (name in oldOn) {
+    if (!on[name]) {
+      event = name.charAt(0) === '!' ? name.slice(1) : name
+      remove(event, oldOn[name].invoker)
+    }
+  }
+}
